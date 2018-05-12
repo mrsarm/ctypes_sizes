acfdb1d454ed5869e316bce3bfc8522621d850ef
Mariano Ruiz <mrsarm@gmail.com>
2018-05-12 19:54:57 -0300

    Fixed license

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 /* main.c
 
-   Copyright (C) 2018 Mariano Ruiz <mrsarm@gmail.com>
-   This file is part of the ctypes_sizes C-project.
+   Copyright (C) 2015-2018 Mariano Ruiz <mrsarm@gmail.com>
+   This file is part of the "Frequency Counter" project.
 
-   The "C-Types sizes" project is free software; you can redistribute it and/or
+   This project is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+   version 3 of the License, or (at your option) any later version.
 
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,7 +14,7 @@
    Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
+   License along with the "C-Types sizes" project; if not, see
    <http://www.gnu.org/licenses/>.  */
 
 
